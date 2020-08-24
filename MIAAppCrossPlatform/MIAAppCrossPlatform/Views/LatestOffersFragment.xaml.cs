305760c3372db26c0d2420e8aac1bf4251d1c77f
Joel Hili <joel.hili@outlook.com>
2020-08-24 09:31:18 +0200

    Added LatestOffersFragment

---
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Views/LatestOffersFragment.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
+
+namespace MIAAppCrossPlatform.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LatestOffersFragment : ContentView
+	{
+		public LatestOffersFragment()
+		{
+			InitializeComponent();
+		}
+	}
+}
