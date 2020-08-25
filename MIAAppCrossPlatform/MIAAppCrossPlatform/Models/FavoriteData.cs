78a65c83e2e27d313245b80fc727fce42680524d
Joel Hili <joel.hili@outlook.com>
2020-08-25 12:30:02 +0200

    Added FavoriteData

---
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Models/FavoriteData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MIAAppCrossPlatform.Models
+{
+	class FavoriteData
+	{
+		private string categoryId;
+		private string partnerId;
+		private string categoryName;
+		private string partnerName;
+		private string partnerImageURL;
+
+		public FavoriteData(string _categoryId, string _partnerId, string _categoryName, string _partnerName, string _partnerImageURL)
+		{
+			categoryId = _categoryId;
+			partnerId = _partnerId;
+			categoryName = _categoryName;
+			partnerName = _partnerName;
+			partnerImageURL = _partnerImageURL;
+		}
+
+		public string getFavCategoryId()
+		{
+			return categoryId;
+		}
+
+		public string getFavPartnerId()
+		{
+			return categoryId;
+		}
+
+		public string getFavCategoryName()
+		{
+			return categoryName;
+		}
+
+		public string getFavPartnerName()
+		{
+			return categoryId;
+		}
+
+		public string getFavPartnerImageUrl()
+		{
+			return partnerImageURL;
+		}
+	}
+}
