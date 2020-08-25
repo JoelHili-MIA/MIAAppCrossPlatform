52c92585a3730d4f7b6293e0f793aaea2125db17
Joel Hili <joel.hili@outlook.com>
2020-08-25 13:07:12 +0200

    Added OfferData

---
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Models/OfferData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MIAAppCrossPlatform.Models
+{
+	class OfferData
+	{
+		private string offerId;
+		private string offerName;
+
+		public OfferData(string _offerId, string _offerName)
+		{
+			offerId = _offerId;
+			offerName = _offerName;
+		}
+
+		public string getOfferId()
+		{
+			return offerId;
+		}
+
+		public string getOfferName()
+		{
+			return offerName;
+		}
+	}
+}
