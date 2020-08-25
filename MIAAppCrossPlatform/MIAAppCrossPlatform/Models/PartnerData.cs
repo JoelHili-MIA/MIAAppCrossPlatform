e0161090a0b6eed0c4b6e1f5cd4244ec9fb75941
Joel Hili <joel.hili@outlook.com>
2020-08-25 13:14:11 +0200

    Added PartnerData

---
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Models/PartnerData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MIAAppCrossPlatform.Models
+{
+	class PartnerData
+	{
+		private string partnerId;
+		private string partnerName;
+		private string partnerImageUrl;
+
+		public PartnerData(string _partnerId, string _partnerName, string _partnerImageUrl)
+		{
+			partnerId = _partnerId;
+			partnerName = _partnerName;
+			partnerImageUrl = _partnerImageUrl;
+		}
+
+		public string getPartnerId()
+		{
+			return partnerId;
+		}
+
+		public string getPartnerName()
+		{
+			return partnerName;
+		}
+
+		public string getPartnerImageUrl()
+		{
+			return partnerImageUrl;
+		}
+	}
+}
