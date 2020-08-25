ecb49da7c9a71471328be976ce046eee67160324
Joel Hili <joel.hili@outlook.com>
2020-08-25 12:56:49 +0200

    Added LatestOffersData and MapData

---
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Models/LatestOffersData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MIAAppCrossPlatform.Models
+{
+	class LatestOffersData:IComparable
+	{
+		private string categoryName;
+		private string partnerName;
+		private string offerName;
+		private string partnerImageUrl;
+		private DateTime dateOfOffer;
+
+		public LatestOffersData(string _categoryName, string _partnerName, string _offerName, string _partnerImageUrl, DateTime _dateOfOffer)
+		{
+			categoryName = _categoryName;
+			partnerName = _partnerName;
+			offerName = _offerName;
+			partnerImageUrl = _partnerImageUrl;
+			dateOfOffer = _dateOfOffer;
+		}
+
+		public string getCategoryName()
+		{
+			return categoryName;
+		}
+
+		public string getPartnerName()
+		{
+			return partnerName;
+		}
+
+		public string getOfferName()
+		{
+			return offerName;
+		}
+
+		public string getPartnerImageUrl()
+		{
+			return partnerImageUrl;
+		}
+
+		public DateTime getDateTime()
+		{
+			return dateOfOffer;
+		}
+
+		public int CompareTo(LatestOffersData latest)
+		{
+			if(getDateTime()==null || latest.getDateTime() == null)
+			{
+				return 0;
+			}
+			return getDateTime().CompareTo(latest.getDateTime());
+		}
+	}
+}
---
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Models/MapData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms.Maps;
+
+namespace MIAAppCrossPlatform.Models
+{
+	class MapData
+	{
+		private static Position mPosition;
+		private static string mTitle;
+
+		public MapData(double _latitude, double _longitude, string _title)
+		{
+			mPosition = new Position(_latitude, _longitude);
+			mTitle = _title;
+		}
+
+		public static Position getPosition()
+		{
+			return mPosition;
+		}
+
+		public static string getTitle()
+		{
+			return mTitle;
+		}
+	}
+}
