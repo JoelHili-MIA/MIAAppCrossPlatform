c4b861591a4d749d45d426e90d9649419daa78d8
Joel Hili <joel.hili@outlook.com>
2020-08-25 11:11:03 +0200

    Added SendEmail

---
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Models/SendEmailTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MIAAppCrossPlatform.Models
+{
+	class SendEmailTask
+	{
+		public static async Task SendEmail( String _subject, String _body, List<string> _recipient)
+		{
+			try
+			{
+				var message = new EmailMessage
+				{
+					Subject = _subject,
+					Body = _body,
+					To = _recipient,
+				};
+
+				await Email.ComposeAsync(message);
+			}
+			catch(FeatureNotEnabledException fnsEx)
+			{
+				//Email not supported on this device
+			}
+			catch(Exception ex)
+			{
+				//Other Exception
+			}
+		}
+	}
+}
--- MIAAppCrossPlatform/MIAAppCrossPlatform/Views/MainActivity.xaml.cs
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Views/MainActivity.xaml.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Core;
+
+using Firebase.Database;
+using Firebase.Database.Query;
 
 namespace MIAAppCrossPlatform.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainActivity : TabbedPage
 	{
+		CategoryFragment categoryFragment;
+		FavoriteFragment favoriteFragment;
+		LatestOffersFragment listOFOfferFragment;
+		ProfileFragment profileFragment;
+		MenuItem prevMenuItem;
+		
 		public MainActivity()
 		{
 			InitializeComponent();
+
+			
 		}
 	}
 }
