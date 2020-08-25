5d73d2cbd703ad9eda8ca5921abc5910e83c61c6
Joel Hili <joel.hili@outlook.com>
2020-08-25 14:31:26 +0200

    Added SavingsSectionData and Started CategoryFragment (filter)

---
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Models/SavingsSectionData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MIAAppCrossPlatform.Models
+{
+	class SavingsSectionData
+	{
+		private string partnerName;
+		private string dateTime;
+		private string savings;
+		private string offer;
+		private string categoryName;
+		private string reference;
+
+		public SavingsSectionData(string _partnerName, string _dateTime, string _savings, string _offer, string _categoryName, string _reference)
+		{
+			partnerName = _partnerName;
+			dateTime = _dateTime;
+			savings = _savings;
+			offer = _offer;
+			categoryName = _categoryName;
+			reference = _reference;
+		}
+
+		public string getPartnerName()
+		{
+			return partnerName;
+		}
+
+		public string getDateTime()
+		{
+			return dateTime;
+		}
+
+		public string getSavings()
+		{
+			return savings;
+		}
+
+		public string getOffer()
+		{
+			return offer;
+		}
+
+		public string getCategoryName()
+		{
+			return categoryName;
+		}
+
+		public string getReference()
+		{
+			return reference;
+		}
+	}
+}
--- MIAAppCrossPlatform/MIAAppCrossPlatform/Views/CategoryFragment.xaml.cs
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Views/CategoryFragment.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MIAAppCrossPlatform.Models;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,20 @@ namespace MIAAppCrossPlatform.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CategoryFragment : ContentView
 	{
+		private DataAdapter dataAdapter;
+		private List<CategoryData> categoryDataList;
+		private ListView listView;
+
+		public Switch catSwitch, partSwitch;
+
+		private Label searchTextValidation;
+		private Entry editText;
+		private string currentUserText;
+
+		//ToDo - Add Database Reference
+
+		ActivityIndicator pb;
+
 		public CategoryFragment()
 		{
 			InitializeComponent();
@@ -20,6 +37,48 @@ namespace MIAAppCrossPlatform.Views
 		private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
 		{
 
+		}
+
+		private void filter(string input)
+		{
+			try
+			{
+				List<CategoryData> filteredList = new List<CategoryData>();
+
+				foreach(CategoryData c in categoryDataList)
+				{
+					if (catSwitch.IsToggled)
+					{
+						if (c.getCategoryName().ToLower().Contains(input.ToLower()))
+						{
+							filteredList.Add(c);
+							searchTextValidation.Text = "";
+						}
+						if(filteredList.Count <= 0)
+						{
+							searchTextValidation.Text = "No results found. Check if your spelling is correct";
+						}
+					}
+					else
+					{
+						if (c.getCatPartnerName().ToLower().Contains(input.ToLower()))
+						{
+							filteredList.Add(c);
+							searchTextValidation.Text = "";
+						}
+						if(filteredList.Count <= 0)
+						{
+							searchTextValidation.Text = "No results found. Check if your spelling is correct";
+						}
+					}
+				}
+
+				listView.ItemsSource = filteredList;
+			}
+			catch(Exception e)
+			{
+
+			}
 		}
 	}
 }
