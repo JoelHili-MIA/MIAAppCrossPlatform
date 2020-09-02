c42b36bbedb5e1b9eb816aaacb3735d052cee02b
Yalcin2 <yalcin.formosa@hotmail.com>
2020-09-02 08:59:49 +0200

    Finished ChangePasswordActivity

--- MIAAppCrossPlatform/MIAAppCrossPlatform/Views/EditDetailsActivity.xaml.cs
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Views/EditDetailsActivity.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Firebase.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,13 @@ namespace MIAAppCrossPlatform.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class EditDetailsActivity : ContentPage
 	{
+		FirebaseClient firebase;
+
 		public EditDetailsActivity()
 		{
 			InitializeComponent();
+
+			firebase = new FirebaseClient("https://mia-database-45d86.firebaseio.com");
 		}
 	}
 }
