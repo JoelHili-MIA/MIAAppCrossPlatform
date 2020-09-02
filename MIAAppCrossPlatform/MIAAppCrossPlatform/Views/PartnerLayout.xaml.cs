ae91a68ddfe3a2326e245bee0cc822d7d42698b6
Yalcin2 <yalcin.formosa@hotmail.com>
2020-09-02 14:26:22 +0200

    Attempt 1: Finished PartnerLayout

--- MIAAppCrossPlatform/MIAAppCrossPlatform/Views/PartnerLayout.xaml.cs
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Views/PartnerLayout.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MIAAppCrossPlatform.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,15 @@ namespace MIAAppCrossPlatform.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PartnerLayout : ViewCell
 	{
+		public string Image { get; set; }
+		public string Name { get; set; }
+
 		public PartnerLayout()
 		{
 			InitializeComponent();
+
+			latestPartner_image.Source = Image;
+			latestOffer_name.Text = Name;
 		}
 	}
 }
