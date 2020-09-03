a2ab8e41ba98be31c980c57803d82c1e0879536e
Yalcin2 <yalcin.formosa@hotmail.com>
2020-09-03 09:41:49 +0200

    Finished Attempt 1: SavingsLayout

--- MIAAppCrossPlatform/MIAAppCrossPlatform/Views/LatestOffersLayout.xaml.cs
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Views/LatestOffersLayout.xaml.cs
@@ -12,9 +12,19 @@ namespace MIAAppCrossPlatform.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LatestOffersLayout : ViewCell
 	{
+		public string CategoryName { get; set; }
+		public string PartnerImage { get; set; }
+		public string OfferName { get; set; }
+		public string PartnerName { get; set; }
+
 		public LatestOffersLayout()
 		{
 			InitializeComponent();
+
+			latestCategory_name.Text = CategoryName;
+			latestPartner_image.Source = PartnerImage;
+			latestOffer_name.Text = OfferName;
+			latestPartner_name.Text = PartnerName;
 		}
 	}
 }
--- MIAAppCrossPlatform/MIAAppCrossPlatform/Views/SavingsLayout.xaml.cs
+++ MIAAppCrossPlatform/MIAAppCrossPlatform/Views/SavingsLayout.xaml.cs
@@ -12,9 +12,24 @@ namespace MIAAppCrossPlatform.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SavingsLayout : ViewCell
 	{
+		public string Reference { get; set; }
+		public string Category { get; set; }
+		public string Partner { get; set; }
+		public string DateAndTime { get; set; }
+		public string SavingsAmount { get; set; }
+		public string Offer { get; set; }
+
 		public SavingsLayout()
 		{
 			InitializeComponent();
+
+			sav_reference.Text = Reference;
+			sav_cat_name.Text = Category;
+			sav_part_name.Text = Partner;
+			d_and_t.Text = DateAndTime;
+			sav_am.Text = SavingsAmount;
+			offer_Name.Text = Offer;
+			
 		}
 	}
 }
